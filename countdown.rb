72b930492bb762ff4695509696e0e98856a4a2b2
kowalix007 L.K <kowalix007@gmail.com>
2020-06-17 07:29:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
- number = 10
+ counter = number
  while number >=0 
   puts "#{number} SECOND(S)!"
-  number -= 1
+  countrer -= 1
   return "HAPPY NEW YEAR!"
  end 
 end
